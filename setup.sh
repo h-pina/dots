a0f9f1bef45388630754cff3004475757b31d9a0
Hugo Pina <hugopina.2000@gmail.com>
2024-11-17 16:44:46 -0300

    Removed debug statements for setup script

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ CUR_DIR=$(pwd)
 #Create symlinks for dotfiles located in .config
 for config in $(find .config/* -maxdepth 1 -type d);
 do
-	echo "ln -sf $CUR_DIR/$config $HOME/.config/$(basename $config)"
+	ln -sf $CUR_DIR/$config $HOME/.config/$(basename $config)
 done
 
 #Create symlinks for dotfiles located in HOME
 for config in $(find .home -maxdepth 1 -type f)
 do
-	echo "ln -sf $CUR_DIR/$config $HOME/$(basename $config)"
+	ln -sf $CUR_DIR/$config $HOME/$(basename $config)
 done
